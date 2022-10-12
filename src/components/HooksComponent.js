1e8548826507ccc6d9cf55cd57a39b968cf23aba
Anurag <anuragchakravorty000@gmail.com>
2022-10-12 13:16:24 +0530

    commit git commit -m "commit"

--- src/components/HooksComponent.js
+++ src/components/HooksComponent.js
@@ -1,56 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function HooksComponent() {
-    const [count , setCount] = useState(2)
-    const decrement = () => {
-        let decCount = count - 1;
-        setCount(decCount)
-    }
-    const increment = () => {
-        let incCount = count + 1;
-        setCount(incCount)
-    }
+  const [count, setCount] = useState(2);
+  const decrement = () => {
+    let decCount = count - 1;
+    setCount(decCount);
+  };
+  const increment = () => {
+    let incCount = count + 1;
+    setCount(incCount);
+  };
   return (
-    <div 
-    >
-
-
-
-        <div
-         style = {{
-            textAlign: 'left'
-        }}>
-
-        <button 
-        onClick={()=> decrement()}
-       >- Decrement</button>
-
-        </div>
-
-        <div
+    <div>
+      <div
         style={{
-            textAlign : 'center'
-        }}>
+          textAlign: "left",
+        }}
+      >
+        <button onClick={() => decrement()}>- Decrement</button>
+      </div>
 
-        <label 
-        >count: {count} </label>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <label>count: {count} </label>
+      </div>
 
-        </div>
-
-        <div
-         style= {{
-            textAlign:'right' 
-        }}>
-
-        <button 
-        onClick={()=> increment()}>+ Increment</button>
-
-        </div>
-       
-        
-        
+      <div
+        style={{
+          textAlign: "right",
+        }}
+      >
+        <button onClick={() => increment()}>+ Increment</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HooksComponent
+export default HooksComponent;
